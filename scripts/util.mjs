406b1b267126b08ebd95e92f60a3bd331b04df54
siuze <windfall@hnu.edu.cn>
2023-03-08 21:56:54 +0800

    init

--- scripts/util.mjs
+++
@@ -1,8 +0,0 @@
-function ensure (result) {
-  if (result.status !== 0) {
-    throw new Error('Command fails.')
-  }
-  return result
-}
-
-export { ensure }
--- src/workerAPI.ts
+++
@@ -1,9 +0,0 @@
-import { LambdaWorker } from '@libreservice/my-worker'
-
-const worker = new LambdaWorker('./worker.js')
-
-const setIME: (ime: string) => Promise<void> = worker.register('setIME')
-const setOption: (option: string, value: boolean) => Promise<void> = worker.register('setOption')
-const process: (input: string) => Promise<string> = worker.register('process')
-
-export { setOption, process, setIME }
